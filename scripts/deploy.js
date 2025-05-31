edd70eaf75f50a29507123ddf6baaf28e974b719
bekezh <64770406+bekezh@users.noreply.github.com>
2025-05-31 23:06:20 +0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying from:", deployer.address);
+  const [deployer] = await ethers.getSigners();  // Получаем адрес деплойера
+  console.log("Deploying from:", deployer.address);  // Логируем его
 
   const stablecoinAddress = "0x3813e82e6f7098b9583FC0F33a962D02018B6803";  // Адрес stablecoin
   const Token = await ethers.getContractFactory("ScooterRentToken");
@@ -8,8 +8,9 @@ async function main() {
   // Передаем два параметра: деплойера как владельца и адрес stablecoin
   const token = await Token.deploy(deployer.address, stablecoinAddress);
   
-  await token.deployed();
-  console.log("ScooterRentToken deployed at:", token.address);
+  await token.deployed();  // Деплоим контракт
+
+  console.log("ScooterRentToken deployed at:", token.address);  // Логируем адрес контракта
 }
 
 main().catch((error) => {
