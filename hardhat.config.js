3e44cb66a813d6b4d48ab8a657958322f4c97108
bekezh <64770406+bekezh@users.noreply.github.com>
2025-05-31 15:42:28 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
     }
   }
