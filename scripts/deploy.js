102215fa14cb68671cc0259afa87f365b0e7c34c
bekezh <64770406+bekezh@users.noreply.github.com>
2025-05-31 15:24:33 +0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying from:", deployer.address);
 
-  const stablecoinAddress = "0xYourTestUSDTToken"; // Replace with actual testnet USDT
+  const stablecoinAddress = "0x3813e82e6f7098b9583FC0F33a962D02018B6803";
   const Token = await ethers.getContractFactory("ScooterRentToken");
   const token = await Token.deploy(stablecoinAddress);
   await token.deployed();
