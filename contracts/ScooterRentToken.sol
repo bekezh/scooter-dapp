0801715e5cd7b4902f6c7920bfc8bc1d10632167
bekezh <64770406+bekezh@users.noreply.github.com>
2025-05-31 23:26:42 +0500

    Update ScooterRentToken.sol

--- contracts/ScooterRentToken.sol
+++ contracts/ScooterRentToken.sol
@@ -9,9 +9,10 @@ contract ScooterRentToken is ERC20, Ownable {
     IERC20 public stablecoin;
 
     // Конструктор теперь принимает два параметра: адрес владельца и адрес stablecoin
-    constructor(address initialOwner, address _stablecoin) ERC20("ScooterRentToken", "SRT") Ownable(initialOwner) {
+    constructor(address initialOwner, address _stablecoin) ERC20("ScooterRentToken", "SRT") {
         _mint(initialOwner, 1_000_000 * 10 ** decimals());  // Минтинг токенов на адрес владельца
         stablecoin = IERC20(_stablecoin);  // Инициализируем stablecoin
+        transferOwnership(initialOwner); // Устанавливаем владельца контракта
     }
 
     // Функция для депозита дивидендов
